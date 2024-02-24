0fbe41b00987feaee036a43730db01b09c3fe51e
nischay-code <nischay.patel.code@gmail.com>
2024-02-24 17:02:56 +0530

    File & Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+      </Box>
     </>
   );
 }
