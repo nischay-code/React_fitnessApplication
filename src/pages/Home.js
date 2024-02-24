21ec616ae76c457562539f5a635bd4a9768086ee
nischay-code <nischay.patel.code@gmail.com>
2024-02-24 17:15:09 +0530

    Hero Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import HeroBanner from "../components/HeroBanner";
+import { Box } from "@mui/material";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+    </Box>
+  );
 };
 
 export default Home;
